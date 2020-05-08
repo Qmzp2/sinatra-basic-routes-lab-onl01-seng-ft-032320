0214c7bba68f457559c113daabc0836dc61defc8
Kevin Mathias <komathias1@gmail.com>
2020-05-08 13:48:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  
+  get 'name' do
+  "Hello, World!"
+end
+
+get 'hometown' do
+  "Hello, World!"
+end
+
+get 'favorite-song' do
+  "Hello, World!"
+end
   
 end
